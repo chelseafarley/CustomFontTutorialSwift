60ad220e234d8f49e34e3cdb45639ea27cf643b1
Chelsea <chelsea@tripwiretech.com>
2023-07-04 19:34:22 +1200

    Initial Commit

---
+++ CustomFontTutorial/CustomFontTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomFontTutorialApp.swift
+//  CustomFontTutorial
+//
+//  Created by Charles Suddens on 4/07/23.
+//
+
+import SwiftUI
+
+@main
+struct CustomFontTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
